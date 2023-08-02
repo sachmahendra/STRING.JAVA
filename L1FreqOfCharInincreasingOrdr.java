3be70a8da8671e363ea3c77b908986f90bcf8250
sachmahendra <123411058+sachmahendra@users.noreply.github.com>
2023-08-02 19:50:07 +0530

    initial.commit
    IT IS A JAVA DSA CODE FOR THE FINDING THE FREQUENCY OF CHARACTER IN INCREASING ORDER

--- L1FreqOfCharInincreasingOrdr.java
+++ L1FreqOfCharInincreasingOrdr.java
@@ -2,13 +2,14 @@ public class L1FreqOfCharInincreasingOrdr {
     public static void main(String[] args) {
         String str = "geeksforgeeks";
         int count[] = new int[26];
-        for(int i=0; i<str.length();i++){
+        for (int i = 0; i < str.length(); i++) {
             count[str.charAt(i) - 'a']++;
 
         }
-        for(int i=0; i<26;i++){
-            if(count[i]>0)
-            System.out.println((char)(i+'a')+ " "+ (count[i]));
+        // to print the frequencies in sorted order
+        for (int i = 0; i < 26; i++) {
+            if (count[i] > 0)
+                System.out.println((char) (i + 'a') + " " + (count[i]));
         }
     }
 }
